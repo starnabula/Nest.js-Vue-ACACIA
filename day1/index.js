c32edb320f08e64f2774e4a4c6bd875f1a7a3c51
THEETHAWACH  BANNAKAN <fordeawq234@gmail.com>
2026-01-20 05:45:57 +0700

    Add new chapters

---
+++ day1/index.js
@@ -0,0 +1,23 @@
+
+
+
+
+// ตัวเดียว
+/*ทั้งหมด*/
+
+
+
+
+console.log('Hello World')
+//ปัญหา
+let x = 10
+x = "JavaScript"
+y = Math.pow(x, 2)
+console.log(x)
+console.log(y)
+
+
+
+let a = "10"
+let b = a + 10
+console.log(b)
---
+++ day1/src/hello.ts
@@ -0,0 +1,14 @@
+
+// ตัวเดียว
+/*ทั้งหมด*/
+
+console.log('Hello World')
+console.log('สวัดดี')
+
+//ปัญหา
+let j: number = 10
+// j = "JavaScript"
+let k: number = Math.pow(j, 2)
+console.log(j)
+console.log(k)
+
