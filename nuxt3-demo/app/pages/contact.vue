d33a1b93bb79240496d61ca243bf1f88f5757178
THEETHAWACH  BANNAKAN <fordeawq234@gmail.com>
2026-01-30 01:19:14 +0700

    Add new chapters

---
+++ nuxt3-demo/app/pages/contact.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1> Contact </h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia repellat iste qui, ullam obcaecati dolor autem animi amet corrupti tempora, laboriosam nobis doloremque perferendis, ea molestiae! Deleniti temporibus aspernatur vitae.</p>
+  </div>
+</template>
+
+<style scoped></style>
