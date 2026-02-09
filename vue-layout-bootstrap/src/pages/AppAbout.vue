076700691d3fdf378e81ef98e8bfd9cda0b1726e
THEETHAWACH  BANNAKAN <fordeawq234@gmail.com>
2026-02-10 03:45:00 +0700

    ลบ

--- vue-layout-bootstrap/src/pages/AppAbout.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <h1>AppAbout</h1>
-</template>
-<style></style>
--- vue-layout-bootstrap/src/pages/AppHome.vue
+++
@@ -1,6 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <h1 class="bg-primary">AppHome</h1>
-</template>
-<style></style>
